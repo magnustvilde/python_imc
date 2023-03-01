d641232ca161478a131f65ffa7928967ca7eafe3
magnustvilde <magnustvilde@gmail.com>
2023-03-01 12:02:27 +0100

    Structure fixing and pdf Python 1

---
+++ python-3/python-3-oppgaver.py
@@ -0,0 +1,58 @@
+# PYTHON 3 - del 1: funksjoner
+# Oppgave 3.1
+# Lag en funksjon som skriver ut teksten “Hello World!”. Kall på funksjonen tre ganger.
+# Oppgave 3.2
+# Beskriv begrepene mtp. funksjoner i Python (gjerne kopier fra et internettsøk, så lenge du er enig i det som står der og forstår det):
+# •	Deklarere en funksjon
+# •	Funksjonskall
+# •	Parametre
+# •	Argumenter
+# •	Returverdi
+# •	Global variabel
+# •	Lokal variabel
+
+# Oppgave 3.3
+# Deklarer en global variabel. Lag en funksjon som skal endre på den globale variabelen. Lag også en lokal variabel inni funksjonen. Kall på funksjonen, og prøv å printe de forskjellige variablene etterpå. Skriv en kommentar i koden for hva som skjer.
+
+# Oppgave 3.4
+# Lag en funksjon som tar inn et tall som parameter. Funksjonen skal skrive “Hello World!” så mange ganger som argumentet tilsier.
+
+# Oppgave 3.5	
+# Lag en funksjon som regner ut lønn fra en timelønn og et antall timer. Timelønn og antall timer skal hentes inn som parametre.
+
+# Oppgave 3.6
+# Lag en funksjon som regner ut gjennomsnittsalderen på et antall mennesker. Antallet mennesker skal sendes inn som parameter. Brukeren skal skrive inn alderen for hvert menneske, og det legges til en totalverdi. Skriv ut gjennomsnittet.
+
+# Oppgave 3.7
+# Lag en funksjon som sjekker om en string inneholder en av bokstavene 'æ', 'ø' eller 'å'. Stringen skal hentes inn som argument. Print ut 'JA' dersom den gjør det.
+# Variabler som kan brukes:
+# tekst = 'Norsk setning med Æ'
+# ny = 'English without the letters'
+
+# Oppgave 3.8
+# Lag en funksjon som skriver ut en bokstav på en bestemt plass i en gitt streng. Posisjonen, eller indeksen, skal bli gitt som argument og stringen skal bli gitt som argument.
+
+# PYTHON 3 - del 2: Funksjoner - returverdi, nøstede funksjonskall
+# Oppgave 3.9
+# Lag en funksjon som spør om fornavn, og så etternavn. Returner hele navnet satt sammen. Lag en variabel som settes lik funksjonskallet.
+
+# Oppgave 3.10
+# Lag en funksjon som tar inn navn som parameter. Spør brukeren (med navn) om alder og bosted. Returner alder og bosted. Kall på funksjonen og lagre returverdiene i variabler.
+
+# Oppgave 3.11
+# Lag en funksjon som tar inn to tall som parametre. Ta inn input fra bruker i form av '/', '*', '//' eller '%'. Funksjonen skal returnere resultatet av den typen matematisk operasjon gjort på de to tallene. Skriv ut resultatet.
+
+#  Oppgave 3.12
+# Lag en funksjon som tar inn en streng. Iterer over stringen og returner den første vokalen du finner. Dersom det ikke finnes noen vokal i strengen, returner en beskrivende tekst.
+
+# Oppgave 3.13
+# Lag en funksjon som tar inn to parametre, timelønn og antall_timer. Funksjonen skal returnere lønn gitt antall timer. Lag så en funksjon som tar inn timelønn og antall timer fra brukeren og returnerer det. Gjør et nøstet funksjonskall (funksjonskall INNI funksjonskall) der den første funksjonen blir kalt først, og den andre etterpå. Print ut resultatet.
+
+# Oppgave 3.14 - UTFORDRING, TAR EN DEL TID
+# Du skal nå lage et terningspill som kombinerer alt du har lært hittil. Husk: Google er din venn!
+# a.	Importer random-modulen (du trenger kun randint).
+# b.	Lag en info()-funksjon som skriver ut info om spillet, samt hvordan man avslutter.
+# c.	Ta inn navnet til en bruker i en funksjon. Returner navnet. Bruk funksjonen til å lage to spillere.t
+# d.	Lag en funksjon som heter trill_terninger. De to brukerne skal trille terninger om og om igjen så lenge brukeren vil. Hold styr på hvor mange poeng de har. Gi ett poeng til den som får høyest terningkast, ingen poeng dersom de får samme terningkast. Returner hvor mange poeng spillerene har etter funksjonen er ferdig.
+# e.	Lag en funksjon som heter start_spill() og som er ansvarlig for å kalle på alle de andre funksjonene.
+# f.	Lag en funksjon som tar inn to parametre, score_en og score_to. Funksjonen skal skrive ut poengscore og hvem som vinner.
